3031f8fa7f048910d0bba29f6fbec41d5842ada9
György Balássy <balassy@users.noreply.github.com>
2016-01-22 14:20:21 +0100

    REFACTORED: The service handles the processing of the response.

--- src/next-ride/controller.js
+++ src/next-ride/controller.js
@@ -1,62 +1,15 @@
 'use strict';
 
-let MomentTimezone = require('moment-timezone');
 let Service = require('./service');
 let Rest = require('../utils/rest');
-
-function formatTime(dateTimeInSeconds) {
-    let gmtDateTime = new Date(dateTimeInSeconds * 1000);
-    let localDateTime = MomentTimezone(gmtDateTime).tz('Europe/Budapest');
-    return localDateTime.format('HH:mm');
-}
 
 module.exports = {
     get: function (request, reply) {
         let stopId = 'BKK_F02296';
         let minutesAfter = 30;
-        
-        let now = formatTime(Date.now() / 1000);
-        
-        let result = {
-            name: '59 villamos',
-            description: 'Vas Gereben utca -> Széll Kálmán tér',
-            now: now,
-            nextRides: [],
-            shortTite: `59 villamos ${now} után`,
-            shortSubTitle: 'Vas G. u. -> Széll K. tér',
-            shortNextRides: ''
-        };
 
         Service.get(stopId, minutesAfter)
-            .then(function (response) {
-                if (response.statusCode !== 200) {
-                    throw new Error(`The webservice returned with a HTTP status code different than 200: ${response.statusCode}`);
-                }
-
-                let body = response.body;
-
-                if (body.version !== 2) {
-                    throw new Error(`The webservice returned a response body with a version different than 2: ${body.version}`);
-                }
-
-                if (body.status !== 'OK') {
-                    throw new Error(`The webservice returned a response body with a status different than OK: ${body.status}`);
-                }
-
-                if (body.code !== 200) {
-                    throw new Error(`The webservice returned a response body with a code different than 200: ${body.code}`);
-                }
-
-                let rides = body.data.entry.stopTimes;
-
-                for (let ride of rides) {
-                    let arrivalTimeInSeconds = ride.predictedArrivalTime || ride.arrivalTime;
-                    let arrivalTimeString = formatTime(arrivalTimeInSeconds);
-                    result.nextRides.push(arrivalTimeString);
-                }
-                
-                result.shortNextRides = result.nextRides.join(', ');
-
+            .then(function (result) {
                 Rest.ok(reply, result);
             })
             .catch(function (err) {
--- src/next-ride/service.js
+++ src/next-ride/service.js
@@ -1,16 +1,69 @@
-var rp = require('request-promise');
+'use strict';
+
+let MomentTimezone = require('moment-timezone');
+let RP = require('request-promise');
+
+function formatTime(dateTimeInSeconds) {
+    let gmtDateTime = new Date(dateTimeInSeconds * 1000);
+    let localDateTime = MomentTimezone(gmtDateTime).tz('Europe/Budapest');
+    return localDateTime.format('HH:mm');
+}
 
 module.exports = {
     get: function (stopId, minutesAfter) {
-        var uri = `http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId=${stopId}&onlyDepartures=true&minutesBefore=0&minutesAfter=${minutesAfter}`;
-        
-        var options = {
+        let now = formatTime(Date.now() / 1000);
+
+        let result = {
+            name: '59 villamos',
+            description: 'Vas Gereben utca -> Széll Kálmán tér',
+            now: now,
+            nextRides: [],
+            shortTite: `59 villamos ${now} után`,
+            shortSubTitle: 'Vas G. u. -> Széll K. tér',
+            shortNextRides: ''
+        };
+
+        let uri = `http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId=${stopId}&onlyDepartures=true&minutesBefore=0&minutesAfter=${minutesAfter}`;
+
+        let options = {
             method: 'GET',
             uri: uri,
             resolveWithFullResponse: true,
             json: true
         };
 
-        return rp(options);
+        return RP(options)
+            .then(function (response) {
+                console.log(response);
+                if (response.statusCode !== 200) {
+                    throw new Error(`The webservice returned with a HTTP status code different than 200: ${response.statusCode}`);
+                }
+
+                let body = response.body;
+
+                if (body.version !== 2) {
+                    throw new Error(`The webservice returned a response body with a version different than 2: ${body.version}`);
+                }
+
+                if (body.status !== 'OK') {
+                    throw new Error(`The webservice returned a response body with a status different than OK: ${body.status}`);
+                }
+
+                if (body.code !== 200) {
+                    throw new Error(`The webservice returned a response body with a code different than 200: ${body.code}`);
+                }
+
+                let rides = body.data.entry.stopTimes;
+
+                for (let ride of rides) {
+                    let arrivalTimeInSeconds = ride.predictedArrivalTime || ride.arrivalTime;
+                    let arrivalTimeString = formatTime(arrivalTimeInSeconds);
+                    result.nextRides.push(arrivalTimeString);
+                }
+
+                result.shortNextRides = result.nextRides.join(', ');
+                
+                return result;
+            });
     }
 };
