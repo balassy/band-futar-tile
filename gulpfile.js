c7f715371d49c3b7614a6017396a49da506ff9fc
György Balássy <balassy@users.noreply.github.com>
2016-01-25 04:07:38 +0100

    ADDED: UrbanJS configuration fine tune - removed the unused features.

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,12 @@
 const gulp = require('gulp');
 const urbanjs = require('urbanjs-tools');
 
+// Disable some UrbanJS features not used in this project.
 urbanjs.initialize(gulp, {
+  jest: false,
+  jsdoc: false,
+  webpack: false
 });
+
+// Although UrbanJS provides a default 'pre-commit' task, it is overridden here to remove the 'test' subtask from it.
+gulp.task('pre-commit', ['analyse']);
