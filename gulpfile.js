ff504c0464898af906ba73cbddcf46f9b0cc995f
György Balássy <balassy@users.noreply.github.com>
2016-01-25 04:21:33 +0100

    CHANGED: Using stricter ESLint ruleset than UrbanJS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const path = require('path');
 const gulp = require('gulp');
 const urbanjs = require('urbanjs-tools');
 
 // Disable some UrbanJS features not used in this project.
 urbanjs.initialize(gulp, {
+  eslint: {
+    configFile: path.join(__dirname, './.eslintrc')
+  },
   jest: false,
   jsdoc: false,
   webpack: false
